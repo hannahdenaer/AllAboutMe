caa11b08c9bef4cf554befd8ed61d27a2e65b8b1
Hannah Denaer <denaerhannah@gmail.com>
2025-04-11 20:01:38 -0500

    Initial Commit

---
+++ AllAboutMe/AllAboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutMeApp.swift
+//  AllAboutMe
+//
+//  Created by Scholar on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AllAboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
